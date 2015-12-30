79a0ce2178111821ecfbf3194ca6719418e977b5
Lex <lexrus@gmail.com>
2015-12-30 17:04:18 +0800

    Add missing comment.

--- Tests/189.swift
+++ Tests/189.swift
@@ -6,6 +6,19 @@
 //  Copyright © 2015 Lex Tang. All rights reserved.
 //
 
+/*
+Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+
+Hint:
+Could you do it in-place with O(1) extra space?
+Related problem: Reverse Words in a String II
+*/
+
 import Foundation
 import XCTest
 
